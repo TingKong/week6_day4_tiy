c86befee8c2cb8d386e14e886d530df3065a7356
Ting <Ting@DESKTOP-MJSRLVH>
2016-11-05 12:24:47 -0400

    Pushing up files

---
+++ ToDoList/ToDoList/Controllers/IDListsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList;
+
+namespace ToDoList.Controllers
+{
+    public class IDListsController : Controller
+    {
+        private ToDoListEntities db = new ToDoListEntities();
+
+        // GET: IDLists
+        public ActionResult Index()
+        {
+            var iDLists = db.IDLists.Include(i => i.TaskManager);
+            return View(iDLists.ToList());
+        }
+
+        // GET: IDLists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IDList iDList = db.IDLists.Find(id);
+            if (iDList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iDList);
+        }
+
+        // GET: IDLists/Create
+        public ActionResult Create()
+        {
+            ViewBag.TaskManagerID = new SelectList(db.TaskManagers, "ID", "Title");
+            return View();
+        }
+
+        // POST: IDLists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TaskManagerID,UserName")] IDList iDList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.IDLists.Add(iDList);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TaskManagerID = new SelectList(db.TaskManagers, "ID", "Title", iDList.TaskManagerID);
+            ViewBag.TaskManagerID = new SelectList(db.TaskManagers, "ID", "Title", iDList.TaskManagerID);
+
+            return View(iDList);
+        }
+
+        // GET: IDLists/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IDList iDList = db.IDLists.Find(id);
+            if (iDList == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TaskManagerID = new SelectList(db.TaskManagers, "ID", "Title", iDList.TaskManagerID);
+            return View(iDList);
+        }
+
+        // POST: IDLists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TaskManagerID,UserName")] IDList iDList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iDList).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TaskManagerID = new SelectList(db.TaskManagers, "ID", "Title", iDList.TaskManagerID);
+            return View(iDList);
+        }
+
+        // GET: IDLists/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IDList iDList = db.IDLists.Find(id);
+            if (iDList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iDList);
+        }
+
+        // POST: IDLists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            IDList iDList = db.IDLists.Find(id);
+            db.IDLists.Remove(iDList);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ToDoList/ToDoList/Controllers/TaskManagersController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList;
+
+namespace ToDoList.Controllers
+{
+    public class TaskManagersController : Controller
+    {
+        private ToDoListEntities db = new ToDoListEntities();
+
+        // GET: TaskManagers
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View(db.TaskManagers.ToList());
+        }
+
+        // GET: TaskManagers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskManager taskManager = db.TaskManagers.Find(id);
+            if (taskManager == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskManager);
+        }
+
+        // GET: TaskManagers/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TaskManagers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Description,DueDate,CompletionDate")] TaskManager taskManager)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaskManagers.Add(taskManager);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taskManager);
+        }
+
+        // GET: TaskManagers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskManager taskManager = db.TaskManagers.Find(id);
+            if (taskManager == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskManager);
+        }
+
+        // POST: TaskManagers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Description,DueDate,CompletionDate")] TaskManager taskManager)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taskManager).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taskManager);
+        }
+
+        // GET: TaskManagers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskManager taskManager = db.TaskManagers.Find(id);
+            if (taskManager == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskManager);
+        }
+
+        // POST: TaskManagers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TaskManager taskManager = db.TaskManagers.Find(id);
+            db.TaskManagers.Remove(taskManager);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
