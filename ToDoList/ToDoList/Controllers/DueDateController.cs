01cb8db613410636e127c67cc0f8a7e24217a3e8
Ting <Ting@DESKTOP-MJSRLVH>
2016-11-06 17:27:26 -0500

    v1 duedate

---
+++ ToDoList/ToDoList/Controllers/DueDateController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class DueDateController : Controller
+    {
+        ToDoListEntities db = new ToDoListEntities();
+
+        // GET: DueDate
+        public ActionResult Index()
+        {
+            var dueDate = from a in db.TaskManagers
+                           join b in db.IDLists on a.ID equals b.TaskManagerID
+
+                           where a.DueDate == DateTime.Today
+
+                          select new 
+                           {
+                              dueDateDisplay= a,
+                              taskDue = a.DueDate 
+                     
+                           };
+
+            return View(dueDate);
+        }
+    }
+}
---
+++ ToDoList/ToDoList/Models/DueDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class DueDate
+    {
+            [DisplayName("Due Today")]
+            public TaskManager dueDateDisplay { get; set; }
+
+
+
+            [DisplayName("Date Rented")]
+            //[PlaceHolder("01/01/2016")]
+            [Required(ErrorMessage = "You must enter a rental date")]
+            [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+             public Nullable<System.DateTime> taskDue { get; set; }
+
+            ToDoListEntities db = new ToDoListEntities();
+
+
+
+        
+    }
+}
